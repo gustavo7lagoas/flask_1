4ef81a094f17e88adde02759a8a176e82e5f6621
Gustavo Fonseca <gustavo.fonseca@farfetch.com>
2018-06-06 15:32:54 +0100

    Add flask website files

---
+++ game_library.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+class Game:
+    def __init__(self, name, category, game_console):
+        self.name = name
+        self.category = category
+        self.game_console = game_console
+
+game1 = Game('Super Mario RPG', 'RPG', 'SNES')
+game2 = Game('Tetris', 'Puzzle', 'Several')
+game3 = Game('Fifa 2018', 'Sports', 'Several')
+game4 = Game('Street Fighter 2', 'Fight', 'Several')
+game_list = [game1, game2, game3, game4]
+
+
+@app.route('/')
+def index():
+    return render_template('list.html', title='my games', game_list=game_list)
+
+
+@app.route('/new')
+def new():
+    return render_template('new.html', title='New Game')
+
+
+@app.route('/create', methods=['POST',])
+def create():
+    game = Game(request.form['name'], request.form['category'],
+        request.form['game_console'])
+    game_list.append(game)
+    return render_template('list.html', title='my games', game_list=game_list)
+
+
+app.run(debug=True)
